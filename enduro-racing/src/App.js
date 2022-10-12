2a1ff3f832f9e5a980cbf4208a218cb69b021527
Luiz Amaro <luizbarueri@gmail.com>
2022-10-12 16:33:20 -0300

    tabela com dados acrecido de db.json

--- enduro-racing/src/App.js
+++ enduro-racing/src/App.js
@@ -2,25 +2,41 @@
 import logo from './logo-enduro-editado.png';
 import './App.css';
 
-const db = require('./db.json');
+const db = require('./api/db.json');
+//const fs = require('fs');
+//const dataJson = JSON.stringify(db)
+//const tabela = JSON.parse(dataJson)
+
+const data = [...db, {"id": "2", "nome": "Ana maria"}]
+const dataJson = JSON.stringify(data)
+const tabela = JSON.parse(dataJson)
+
+//fs.writeFileSync('db.json', JSON.stringify(data))
+const result = data.find((cliente) => cliente.id === '2')
 
 function App() {
-  const result = db.find((cliente) => cliente.id === '1')
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          resultado API <code>{JSON.stringify(result)}</code>
+          Resultado API <code>{JSON.stringify(result)}</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <div>
+        <table border = '1'>
+          <tr><td>{tabela[0].id}</td><td>{tabela[0].nome}</td><td><a
+                className="App-link"
+                href="https://ressgate-amigo.vercel.app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Comprar Jogo Resgate Amigo -  href="https://ressgate-amigo.vercel.app/"
+              </a></td></tr>
+              <tr><td>{tabela[1].id}</td><td>{tabela[1].nome}</td><td></td></tr>
+        </table>
+      </div>
+
       </header>
     </div>
   );
---
+++ enduro-racing/src/pages/comprar.js
@@ -0,0 +1,5 @@
+export default function Comprar() {
+    return (
+        <div>Realizando compra</div>
+    )
+}
